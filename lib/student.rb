3c39a57d007025734b4116893abb4264b6f64962
Zachary <zvcoding@gmail.com>
2021-11-07 19:23:09 -0500

    commit tree e61c08d7984201074f0974a851d1d8aaaacfb147

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(stringOfKnowledge)
+    @knowledge << stringOfKnowledge
+  end 
+
+  def knowledge
+    @knowledge
+  end 
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+class User
+  # attr_accessor :first_name, :last_name
+
+  attr_accessor :first_name, :last_name
+
+  # def initialize(firstname, lastname)
+  #   @first_name = firstname
+  #   @last_name = lastname
+  # end 
+
+end
